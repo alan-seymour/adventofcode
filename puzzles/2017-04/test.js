3786b41740ff87eea649d45d4346433b16f81336
Alan Seymour <alan.seymour@navitas.com>
2017-12-04 13:46:46 +0800

    2017 day 4

---
+++ puzzles/2017-04/test.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testCases: [
+    {
+      part: 1,
+      input: `aa bb cc dd ee
+      aa bb cc dd aa
+      aa bb cc dd aaa`,
+      output: 2,
+    },
+    {
+      part: 2,
+      input: `abcde fghij
+      abcde xyz ecdab
+      a ab abc abd abf abj
+      iiii oiii ooii oooi oooo
+      oiii ioii iioi iiio`,
+      output: 3,
+    },
+  ],
+};
